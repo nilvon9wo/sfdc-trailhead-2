0c67dda6b585d58937a28971b0a6f54877dd6ec2
Brian Kessler <kessler.bm@gmail.com>
2017-02-01 22:18:44 +0100

    Added alternative handlers for helloMessageInteractive

--- src/aura/helloMessageInteractive/helloMessageInteractiveController.js
+++ src/aura/helloMessageInteractive/helloMessageInteractiveController.js
@@ -1,7 +1,14 @@
 ({
 	handleClick : function(component, event) {
 		var buttonClicked = event.getSource();
-        var buttonMessage = buttonClicked.get("v.label");
-        component.set("v.message", buttonMessage);
+        var buttonMessage = buttonClicked.get('v.label');
+        component.set('v.message', buttonMessage);
+	},
+	handleClick2 : function(component, event) {
+        var newMessage = event.getSource().get('v.label');
+        component.set('v.message', newMessage);
+	},
+	handleClick3 : function(component, event) {
+        component.set('v.message', event.getSource().get('v.label'));
 	}
 })
