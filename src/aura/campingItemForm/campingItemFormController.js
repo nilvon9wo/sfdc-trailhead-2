9281ac239f0b08c48f9172e56f51a764bc41df8d
Brian Kessler <kessler.bm@gmail.com>
2017-02-03 22:07:43 +0100

    Cleaned up error messages after errors are fixed; also some refactoring

--- src/aura/campingItemForm/campingItemFormController.js
+++ src/aura/campingItemForm/campingItemFormController.js
@@ -5,24 +5,9 @@
         	&& helper.isNotBlank(component, 'Price', 'price');
         
         if (isValid) {
-            var newItem = component.get("v.newItem");
-            console.info("Create item: " + JSON.stringify(newItem));
-
-            //helper.createItem(component, newItem);
-        	var newItem = JSON.parse(JSON.stringify(newItem));
-            
-            var items = component.get("v.items");
-        	items.push(newItem);
-        	component.set("v.items", items);
-            
-            var blankItem = {
-                sobjectType: 'Camping_Item__c', 
-                Name: '',
-                Quantity__c: 0, 
-                Price__c: 0,
-                Packed__c: false
-            };                                   }
-            component.set("v.newItem", JSON.parse(JSON.stringify(blankItem)));
+        	var newItem = component.get('v.newItem');
+            helper.createItem(component, newItem);
+            helper.resetForm(component);
         }
     }
 })
--- src/aura/campingItemForm/campingItemFormHelper.js
+++ src/aura/campingItemForm/campingItemFormHelper.js
@@ -1,8 +1,8 @@
 ({
-    createItem: function(component, item) {
+    createItem: function(component, newItem) {
+        var newItemClone = JSON.parse(JSON.stringify(newItem));
         var items = component.get("v.items");
-        var newItem = JSON.parse(JSON.stringify(item));
-        items.push(newItem);
+        items.push(newItemClone);
         component.set("v.items", items);
     },
 	
@@ -13,12 +13,28 @@
             field.set('v.errors', [{message: errorMessage}]);
             return false;
         }
+        else {
+            field.set('v.errors', []);
+        }
         return true;
 	},
+    
     hasValue : function(value) {
         return !($A.util.isEmpty(value));
     },
+    
     isNotBlank: function(component, label, fieldName) {
         return this.validate(component, fieldName, label + ' can\'t be blank.', this.hasValue);
+    },
+    
+    resetForm: function(component) {
+			var blankItem = {
+                sobjectType: 'Camping_Item__c', 
+                Name: '',
+                Quantity__c: 0, 
+                Price__c: 0,
+                Packed__c: false
+            };                                   
+            component.set('v.newItem', JSON.parse(JSON.stringify(blankItem)));	
     }
 })
